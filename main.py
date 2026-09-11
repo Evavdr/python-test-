93c5b6ee3c26de9a62cb96fe388c76646aab0c55
eva <eva.vande-rosieren@ipsa.fr>
2026-09-11 11:34:17 +0200

    setup ui

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-print("Bonjour machin")
+import sys
+from PySide6.QtWidgets import QApplication, QWidget, QMessageBox
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtCore import QFile
 
-print ("abc")
-print("hello")
+app = QApplication(sys.argv)
 
+loader = QUiLoader()
+f = QFile("main.ui")
+f.open(QFile.ReadOnly)
+w = loader.load(f)
+f.close()
+
+w.btnHello.clicked.connect(lambda: QMessageBox.information(w, "Hello", "Hello World"))
+w.show()
+sys.exit(app.exec())
